c4371fc08177c89c28c840781d59191d94c591ea
deretame <81013544+deretame@users.noreply.github.com>
2025-01-05 17:45:50 +0800

    # 优化 优化上传头像的图片质量

--- lib/page/setting/view/bika_setting.dart
+++ lib/page/setting/view/bika_setting.dart
@@ -144,19 +144,10 @@ class _BikaSettingPageState extends State<BikaSettingPage> {
             return;
           }
         }
-        debugPrint('Selected image path: $selectedImages'); // 输出选择的图片路径
-        File originalFile = File(selectedImages);
-        List<int> imageBytes = await originalFile.readAsBytes();
 
-        var decodedImage = decodeImage(Uint8List.fromList(imageBytes));
-
-        List<int> compressedBytes = encodeJpg(decodedImage!, quality: 50);
-
-        // 将字节数组转换为 Base64 字符串
-        final String base64String = base64Encode(compressedBytes);
         EasyLoading.show(status: '正在上传头像...');
         try {
-          await updateAvatar(base64String);
+          await updateAvatar(await compressImage(File(selectedImages)));
           EasyLoading.showInfo("成功上传头像");
         } catch (e) {
           EasyLoading.showError("上传头像失败: ${e.toString()}");
@@ -178,6 +169,37 @@ class _BikaSettingPageState extends State<BikaSettingPage> {
         ],
       ),
     );
+  }
+
+  Future<String> compressImage(File originalFile) async {
+    List<int> imageBytes = await originalFile.readAsBytes();
+    var decodedImage = decodeImage(Uint8List.fromList(imageBytes));
+
+    // 将原始图像转换为 base64 字符串
+    String base64String = base64Encode(imageBytes);
+    int originalBase64Length = base64String.length;
+
+    // 计算压缩比
+    double compressionRatio = 1400000 / originalBase64Length;
+    int quality = (compressionRatio * 100).toInt();
+    quality = quality.clamp(1, 100); // 确保 quality 在 1 到 100 之间
+
+    // 压缩图像
+    var compressedBytes = encodeJpg(decodedImage!, quality: quality);
+
+    // 再次检查压缩后的 base64 长度
+    String compressedBase64String = base64Encode(compressedBytes);
+    if (compressedBase64String.length > 1400000) {
+      // 如果仍然超过长度限制，可以进一步降低 quality
+      quality = (quality * 0.9).toInt(); // 降低 quality 10%
+      compressedBytes = encodeJpg(decodedImage, quality: quality);
+    }
+
+    // 最终压缩后的 base64 字符串
+    String finalBase64String = base64Encode(compressedBytes);
+    debugPrint('Final Base64 Length: ${finalBase64String.length}');
+
+    return finalBase64String;
   }
 
   Widget _changeBriefIntroduction() {

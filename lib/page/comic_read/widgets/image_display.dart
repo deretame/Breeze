575c94bb72f61936fbb2e3b9094116b3c1fd26c1
deretame <81013544+deretame@users.noreply.github.com>
2025-01-24 14:18:47 +0800

    修复漫画阅读界面阅览图片大图造成的闪烁问题

--- lib/page/comic_read/widgets/image_display.dart
+++ lib/page/comic_read/widgets/image_display.dart
@@ -17,47 +17,50 @@ class ImageDisplay extends StatefulWidget {
 class _ImageDisplayState extends State<ImageDisplay> {
   double imageWidth = screenWidth;
   double imageHeight = screenWidth;
-
-  Future<ImageInfo>? _imageInfoFuture;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _imageInfoFuture = _getImageResolution(widget.imagePath);
+    _getImageResolution(widget.imagePath);
   }
 
-  Future<ImageInfo> _getImageResolution(String imagePath) async {
-    final Completer<ImageInfo> completer = Completer();
+  Future<void> _getImageResolution(String imagePath) async {
     final Image image = Image.file(File(imagePath));
+    final ImageStream stream = image.image.resolve(ImageConfiguration());
 
-    image.image.resolve(ImageConfiguration()).addListener(
-      ImageStreamListener((ImageInfo imageInfo, _) {
-        completer.complete(imageInfo);
-      }),
+    stream.addListener(
+      ImageStreamListener(
+        (ImageInfo imageInfo, _) {
+          if (mounted) {
+            setState(() {
+              imageWidth = imageInfo.image.width.toDouble();
+              imageHeight = imageInfo.image.height.toDouble();
+              _isLoading = false;
+            });
+          }
+        },
+        onError: (exception, stackTrace) {
+          if (mounted) {
+            setState(() {
+              _isLoading = false;
+            });
+          }
+        },
+      ),
     );
-
-    return completer.future;
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<ImageInfo>(
-      future: _imageInfoFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done &&
-            snapshot.hasData) {
-          final imageInfo = snapshot.data!;
-          imageWidth = imageInfo.image.width.toDouble();
-          imageHeight = imageInfo.image.height.toDouble();
-        }
-
-        return AspectRatio(
-          aspectRatio: imageWidth / imageHeight,
-          child: imageWidth != screenWidth && imageHeight != screenWidth
-              ? Image.file(File(widget.imagePath), fit: BoxFit.cover)
-              : Container(color: Color(0xFF2D2D2D)),
-        );
-      },
-    );
+    return _isLoading
+        ? Container(color: Color(0xFF2D2D2D)) // 加载中的占位符
+        : AspectRatio(
+            aspectRatio: imageWidth / imageHeight,
+            child: Image.file(
+              File(widget.imagePath),
+              fit: BoxFit.cover,
+            ),
+          );
   }
 }

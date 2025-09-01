6cfb6b452c8a553bb87176778fb648b0437ddbc6
windy <81013544+deretame@users.noreply.github.com>
2025-09-01 22:06:25 +0800

    升级依赖，安卓最低版本升级为7.0，修复内存泄漏问题

--- lib/page/comic_read/widgets/app_bar.dart
+++ lib/page/comic_read/widgets/app_bar.dart
@@ -93,46 +93,47 @@ class _SettingsDialogState extends State<SettingsDialog> {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('阅读模式设置'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          _buildModeOption('从上到下', 'topToBottom'),
-          _buildModeOption('从左到右', 'leftToRight'),
-          _buildModeOption('从右到左', 'rightToLeft'),
-        ],
+      content: RadioGroup<String>(
+        groupValue: _selectedMode,
+        onChanged: (String? value) {
+          if (value != null) {
+            _onModeChanged(value);
+          }
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _buildModeOption('从上到下', 'topToBottom'),
+            _buildModeOption('从左到右', 'leftToRight'),
+            _buildModeOption('从右到左', 'rightToLeft'),
+          ],
+        ),
       ),
       actions: [
-        TextButton(
-          child: const Text('关闭'),
-          onPressed: () => context.pop(), // 关闭对话框
-        ),
+        TextButton(child: const Text('关闭'), onPressed: () => context.pop()),
       ],
     );
+  }
+
+  void _onModeChanged(String newValue) {
+    setState(() => _selectedMode = newValue); // 更新选中的阅读模式
+
+    if (newValue == 'topToBottom') {
+      globalSetting.setReadMode(0);
+    } else if (newValue == 'leftToRight') {
+      globalSetting.setReadMode(1);
+    } else if (newValue == 'rightToLeft') {
+      globalSetting.setReadMode(2);
+    }
+    widget.changePageIndex(2);
   }
 
   // 构建阅读模式选项
   Widget _buildModeOption(String label, String value) {
     return ListTile(
       title: Text(label),
-      leading: Radio<String>(
-        value: value,
-        groupValue: _selectedMode,
-        onChanged: (String? newValue) {
-          // logger.d('newValue: $newValue');
-          setState(() => _selectedMode = newValue!); // 更新选中的阅读模式
-        },
-      ),
-      onTap: () {
-        setState(() => _selectedMode = value); // 更新选中的阅读模式
-        if (value == 'topToBottom') {
-          globalSetting.setReadMode(0);
-        } else if (value == 'leftToRight') {
-          globalSetting.setReadMode(1);
-        } else if (value == 'rightToLeft') {
-          globalSetting.setReadMode(2);
-        }
-        widget.changePageIndex(2);
-      },
+      leading: Radio<String>(value: value),
+      onTap: () => _onModeChanged(value),
     );
   }
 }
--- lib/page/comic_read/widgets/image_display.dart
+++ lib/page/comic_read/widgets/image_display.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-
 import '../../../config/global/global.dart';
 
 class ImageDisplay extends StatefulWidget {
@@ -18,39 +16,53 @@ class ImageDisplay extends StatefulWidget {
   State<ImageDisplay> createState() => _ImageDisplayState();
 }
 
-class _ImageDisplayState extends State<ImageDisplay> {
+class _ImageDisplayState extends State<ImageDisplay>
+    with AutomaticKeepAliveClientMixin {
+  ImageStream? _imageStream;
+  late final ImageStreamListener _imageListener;
+
   bool get isColumn => widget.isColumn;
 
   double imageWidth = screenWidth;
   double imageHeight = screenWidth;
 
   @override
+  bool get wantKeepAlive => true;
+
+  @override
   void initState() {
     super.initState();
-    // logger.d(isColumn);
-    if (isColumn) _getImageResolution(widget.imagePath);
+    _imageListener = ImageStreamListener((ImageInfo imageInfo, _) {
+      if (mounted) {
+        setState(() {
+          imageWidth = imageInfo.image.width.toDouble();
+          imageHeight = imageInfo.image.height.toDouble();
+        });
+      }
+    });
   }
 
-  void _getImageResolution(String imagePath) {
-    final Image image = Image.file(File(imagePath));
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (isColumn) {
+      final imageProvider = FileImage(File(widget.imagePath));
+      _imageStream = imageProvider.resolve(
+        createLocalImageConfiguration(context),
+      );
+      _imageStream!.addListener(_imageListener);
+    }
+  }
 
-    // 监听图片解析完成
-    image.image
-        .resolve(ImageConfiguration())
-        .addListener(
-          ImageStreamListener((ImageInfo imageInfo, _) {
-            if (mounted) {
-              setState(() {
-                imageWidth = imageInfo.image.width.toDouble();
-                imageHeight = imageInfo.image.height.toDouble();
-              });
-            }
-          }),
-        );
+  @override
+  void dispose() {
+    _imageStream?.removeListener(_imageListener);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Container(
       color: Colors.black,
       width: screenWidth,

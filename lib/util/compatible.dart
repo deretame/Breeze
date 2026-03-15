6bfc80c16d7c5d66c443e87eda6517d2e6eb71a3
windy <deretame@outlook.com>
2026-03-15 12:20:26 +0800

    优化兼容性问题

--- lib/util/compatible.dart
+++ lib/util/compatible.dart
@@ -1,8 +1,19 @@
 // 用来提供兼容操作的
 
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+import 'package:zephyr/util/get_path.dart';
+import 'package:zephyr/util/sundry.dart';
 
 Future<void> compatibleInit() async {
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setString('compatible_version', "v1");
+  final basePath = await getFilePath();
+  final compatiblePath = p.join(basePath, 'compatible');
+  // 检查目录是否存在，如果不存在则创建
+  if (!await Directory(compatiblePath).exists()) {
+    await Directory(compatiblePath).create(recursive: true);
+  }
+  final data = {"compatible_version": "v1"}.toJson();
+  final compatibleFile = File(p.join(compatiblePath, 'compatible.json'));
+  await compatibleFile.writeAsString(data);
 }

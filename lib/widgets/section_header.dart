6faf062f02ffa7b80b2e92e49ebd7e1dce201ea0
windy <deretame@outlook.com>
2026-04-25 23:11:27 +0800

    优化操作体验

--- lib/widgets/section_header.dart
+++ lib/widgets/section_header.dart
@@ -29,42 +29,43 @@ class SectionHeader extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
         ),
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        child: Row(
-          children: [
-            Expanded(
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: context.theme.colorScheme.onSurface,
-                ),
-              ),
-            ),
-            if (subtitle.trim().isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(right: 8),
+        child: GestureDetector(
+          onTap: canNavigate ? onTap : null,
+          child: Row(
+            children: [
+              Expanded(
                 child: Text(
-                  subtitle,
+                  title,
                   style: TextStyle(
-                    color: context.theme.colorScheme.onSurfaceVariant,
-                    fontSize: 12,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: context.theme.colorScheme.onSurface,
                   ),
                 ),
               ),
-            if (canNavigate)
-              GestureDetector(
-                onTap: onTap,
-                child: Icon(
+              if (subtitle.trim().isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(right: 8),
+                  child: Text(
+                    subtitle,
+                    style: TextStyle(
+                      color: context.theme.colorScheme.onSurfaceVariant,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              if (canNavigate)
+                Icon(
                   Icons.arrow_forward_ios,
                   size: 16,
                   color: context.theme.colorScheme.onSurface.withValues(
                     alpha: 0.5,
                   ),
                 ),
-              ),
-            if (canNavigate) const SizedBox(width: 5),
-          ],
+
+              if (canNavigate) const SizedBox(width: 5),
+            ],
+          ),
         ),
       ),
     );

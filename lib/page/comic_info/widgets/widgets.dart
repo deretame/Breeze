5d27ecd3aee109e2da11dca7ad49a7ea4b8ba077
deretame <deretame@outlook.com>
2026-09-25 21:35:06 +0800

    chore(comic_info): 删除已注释掉的 export_type_dialog 死代码及导出

--- lib/page/comic_info/widgets/widgets.dart
+++ lib/page/comic_info/widgets/widgets.dart
@@ -4,7 +4,6 @@ export 'package:zephyr/page/comic_info/widgets/cover.dart';
 export 'package:zephyr/page/comic_info/widgets/creator_info.dart';
 export 'package:zephyr/page/comic_info/widgets/eps.dart';
 export 'package:zephyr/page/comic_info/widgets/episode_download_controller.dart';
-export 'package:zephyr/page/comic_info/widgets/export_type_dialog.dart';
 export 'package:zephyr/page/comic_info/widgets/particulars.dart';
 export 'package:zephyr/page/comic_info/widgets/recommend.dart';
 export 'package:zephyr/page/comic_info/widgets/comic_preview.dart';
